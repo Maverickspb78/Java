af008a6741f1b62f67797344735fe2ed7930cd53
Mav <maverickspb78@gmail.com>
2020-10-31 12:46:50 +0300

    Домашняя работа 5 урок

--- Java1/src/main/java/lesson5/Person.java
+++ Java1/src/main/java/lesson5/Person.java
@@ -1,17 +1,37 @@
 package lesson5;
 
 public class Person {
-    String name;
-    String position;
-    String email;
-    String phone;
-    long salary;
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private long salary;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
 
     public int getAge() {
         return age;
     }
 
-    int age;
 
     public Person(String name, String position, String email, String phone,
                   long salary, int age) {
